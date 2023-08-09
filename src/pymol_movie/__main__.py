7c5348ebb6b14c1a117d5384ed7ea7502528b504
turnyanskiy <turnyanskiy.luke@gmail.com>
2023-08-09 13:48:15 +0100

    pyproject

---
+++ src/pymol_movie/__main__.py
@@ -0,0 +1,31 @@
+"""Program entry point."""
+from .cli import parsers
+from .movie import movie, loaders
+from typing import cast
+
+
+def main() -> None:
+    """Entry Function."""
+    args = parsers.parse_args()
+
+    yaml_dict: dict = cast(dict, parsers.parse_yaml(args.yaml_filepath))
+
+    print(yaml_dict)
+
+    # Loads all objects
+    for pymol_object in yaml_dict["setup"]["objects"]:
+        loaders.ObjectLoader(
+            pymol_object["directory"], pymol_object["name"]
+        ).load_up_to_state(pymol_object["states"])
+
+    # Creates scenes
+    movie_maker = movie.MovieMaker()
+    for scene in yaml_dict["scenes"]:
+        movie_maker.setup_scene(scene)
+
+    # Produce movie
+    movie_maker.produce_movie(yaml_dict["produce"])
+
+
+if __name__ == "__main__":
+    main()
